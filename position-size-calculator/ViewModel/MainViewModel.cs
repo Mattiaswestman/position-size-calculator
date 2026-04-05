aff76707554c9d2f4d4cd0d545b9c299fbe6fac1
Mattias Westman <mattiaswestman@outlook.com>
2026-04-05 16:06:02 +0200

    Added stroke around Buttons & Entries. Added changing of background color for long button & short button.

--- position-size-calculator/ViewModel/MainViewModel.cs
+++ position-size-calculator/ViewModel/MainViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System.Diagnostics;
+using Microsoft.Maui.Graphics;
 
 namespace PositionSizeCalculator.ViewModel
 {
     public partial class MainViewModel : ObservableObject
     {
+        public Color LongButtonColor => IsLong ? GetStaticResourceColor("Green") : GetStaticResourceColor("ForegroundColor");
+        public Color ShortButtonColor => !IsLong ? GetStaticResourceColor("Red") : GetStaticResourceColor("ForegroundColor");
+
         public double AccountSizeValue
         {
             get => accountSizeValue;
@@ -64,13 +67,14 @@ namespace PositionSizeCalculator.ViewModel
         private double sharesValue;
         [ObservableProperty]
         private double riskValue;
+        [ObservableProperty]
+        private bool isLong = true;
 
         private double accountSizeValue;
         private double riskPercentage;
         private double entryPrice;
         private double stopLossPrice;
-        private bool isLong;
-
+        
         public MainViewModel()
         {
         }
@@ -78,18 +82,24 @@ namespace PositionSizeCalculator.ViewModel
         [RelayCommand]
         private void ToggleLong()
         {
-            isLong = true;
+            if (!IsLong)
+            {
+                IsLong = true;
+            }
         }
 
         [RelayCommand]
         private void ToggleShort()
         {
-            isLong = false;
+            if (IsLong)
+            {
+                IsLong = false;
+            }
         }
 
         private void TryCalculatePositionSize()
         {
-            if (accountSizeValue == 0 || riskPercentage == 0 || entryPrice == 0 || stopLossPrice == 0)
+            if (!AreAllValuesSet())
             {
                 return;
             }
@@ -99,6 +109,26 @@ namespace PositionSizeCalculator.ViewModel
             SharesAmount = (int)(RiskValue / riskPerShare);
             SharesValue = SharesAmount * entryPrice;
             Math.Round(SharesValue, 2);
+        }
+
+        private static Color GetStaticResourceColor(string key)
+        {
+            if (Application.Current.Resources.TryGetValue(key, out var value) && value is Color color)
+            {
+                return color;
+            }
+            return Colors.Transparent;
+        }
+
+        private bool AreAllValuesSet()
+        {
+            return (accountSizeValue != 0 && riskPercentage != 0 && entryPrice != 0 && stopLossPrice != 0);
+        }
+
+        partial void OnIsLongChanged(bool value)
+        {
+            OnPropertyChanged(nameof(LongButtonColor));
+            OnPropertyChanged(nameof(ShortButtonColor));
         }
     }
 }
